230485359b79bfdc5af44d489163ac73f868310f
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2025-10-19 14:53:33 +0200

    wip

--- scripts/clone-repo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -n "${settings.token}" ]; then
-    git clone -b ${settings.branch} https://${settings.token}@github.com/${settings.repo}.git /home/jelastic/app
-else
-    git clone -b ${settings.branch} https://github.com/${settings.repo}.git /home/jelastic/app
-fi
--- scripts/create-env-file.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Create an .env file
-cat > /home/jelastic/.env "${env}"
